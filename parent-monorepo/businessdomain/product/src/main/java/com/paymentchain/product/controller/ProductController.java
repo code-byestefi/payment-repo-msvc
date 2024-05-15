8b4f8087bba1d28cc687b5a96c7c490b46931666
Estefi Turin <estefi.turin@gmail.com>
2024-05-15 15:07:13 -0300

    product mvc

---
+++ parent-monorepo/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.paymentchain.product.controller;
+
+import com.paymentchain.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+}
