c42d5e962d9c7ee27c3a3593206b8811ba6a2615
Estefi Turin <estefi.turin@gmail.com>
2024-05-15 21:39:20 -0300

    ++webclient costumer&product

--- parent-monorepo/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerController.java
+++ parent-monorepo/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerController.java
@@ -1,14 +1,26 @@
 package com.paymentchain.customer.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.entities.CustomerProduct;
 import com.paymentchain.customer.repository.CustomerRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/customer")
@@ -17,23 +29,43 @@ public class CustomerController {
     @Autowired
     CustomerRepository customerRepository;
 
+    private final WebClient.Builder webClientBuilder;
+
+    public CustomerController(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    //webClient requires HttpClient library to work propertly
+    HttpClient client = HttpClient.create()
+            //Connection Timeout: is a period within which a connection between a client and a server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            //Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain
+            //period of time, while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
+
+
+
     @GetMapping
     public List<Customer> list(){
         return customerRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> get(@PathVariable Long id){
-        Optional<Customer> customer = customerRepository.findById(id);
-        if (customer.isPresent()) {
-            return new ResponseEntity<>(customer.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public Customer get(@PathVariable(name = "id") long id) {
+        return customerRepository.findById(id).get();
     }
 
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Customer customer){
+        customer.getProducts().forEach(product -> product.setCustomer(customer));
         Customer save = customerRepository.save(customer);
         return ResponseEntity.ok(save);
     }
@@ -57,6 +89,36 @@ public class CustomerController {
     public ResponseEntity<?> delete(@PathVariable Long id){
         customerRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/full")
+    public Customer getByCode(@RequestParam(name = "code") String code) {
+        Customer customer = customerRepository.findByCode(code);
+        if (customer != null) {
+            List<CustomerProduct> products = customer.getProducts();
+
+            //for each product find it name
+            products.forEach(x -> {
+                String productName = getProductName(x.getProductId());
+                x.setProductName(productName);
+            });
+
+        }
+        return customer;
+    }
+
+
+
+    private String getProductName(long id) {
+        WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://localhost:8083/product")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+                .build();
+        JsonNode block = build.method(HttpMethod.GET).uri("/" + id)
+                .retrieve().bodyToMono(JsonNode.class).block();
+        String name = block.get("name").asText();
+        return name;
     }
 
 
--- parent-monorepo/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductController.java
+++ parent-monorepo/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.paymentchain.product.controller;
 
+import com.paymentchain.product.entities.Product;
 import com.paymentchain.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -11,4 +16,35 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @GetMapping()
+    public List<Product> list() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product get(@PathVariable(name = "id") long id) {
+        return productRepository.findById(id).get();
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product) {
+        Product save = productRepository.save(product);
+        return ResponseEntity.ok(save);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Product product) {
+        Product save = productRepository.save(product);
+        return ResponseEntity.ok(save);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        product.ifPresent(value -> productRepository.delete(value));
+        return ResponseEntity.ok().build(   );
+    }
+
 }
