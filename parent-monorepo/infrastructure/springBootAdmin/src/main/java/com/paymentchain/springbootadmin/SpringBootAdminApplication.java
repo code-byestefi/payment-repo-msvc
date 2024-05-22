1be0a53d5cc34db1faf959919e8ad53df3c84a0f
Estefi Turin <estefi.turin@gmail.com>
2024-05-21 21:34:23 -0300

    ++changes

--- parent-monorepo/infrastructure/springBootAdmin/src/main/java/com/paymentchain/springbootadmin/SpringBootAdminApplication.java
+++ parent-monorepo/infrastructure/springBootAdmin/src/main/java/com/paymentchain/springbootadmin/SpringBootAdminApplication.java
@@ -1,8 +1,14 @@
 package com.paymentchain.springbootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminApplication {
 
